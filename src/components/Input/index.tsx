3653fe8aab7cc6ec47615fd592983c7b2fb0559d
Patrick Morais <ppternunes@gmail.com>
2021-10-01 16:24:58 -0300

    refactored modalAddFood component / iox input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -11,8 +11,8 @@ import { Container } from './styles';
 
 interface InputProps {
   name: string;
-  icon: any;
-  rest: any;
+  icon?: any;
+  placeholder: string;
 }
 
 const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
--- src/components/ModalAddFood/index.tsx
+++ src/components/ModalAddFood/index.tsx
@@ -4,27 +4,28 @@ import { FiCheckSquare } from 'react-icons/fi';
 import { Form } from './styles';
 import Modal from '../Modal';
 import Input from '../Input';
+import { FormHandles } from '@unform/core';
 
-class ModalAddFood extends Component {
-  constructor(props) {
-    super(props);
+interface ModalAddFoodProps {
+  isOpen: boolean;
+  setIsOpen: () => void;
+  handleAddFood: () => void;
+}
 
-    this.formRef = createRef();
-  }
+function ModalAddFood(props: ModalAddFoodProps) {
+    const formRef = createRef<FormHandles>();
 
-  handleSubmit = async data => {
-    const { setIsOpen, handleAddFood } = this.props;
+  const handleSubmit = async data => {
+    const { setIsOpen, handleAddFood } = props;
 
     handleAddFood(data);
     setIsOpen();
   };
-
-  render() {
-    const { isOpen, setIsOpen } = this.props;
+    const { isOpen, setIsOpen } = props;
 
     return (
       <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-        <Form ref={this.formRef} onSubmit={this.handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Novo Prato</h1>
           <Input name="image" placeholder="Cole o link aqui" />
 
@@ -41,7 +42,6 @@ class ModalAddFood extends Component {
         </Form>
       </Modal>
     );
-  }
 };
 
 export default ModalAddFood;
