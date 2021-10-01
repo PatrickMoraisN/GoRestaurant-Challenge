541bfd92dcfcb9409b51d47ee1fcfb51117c2b74
Patrick Morais <ppternunes@gmail.com>
2021-10-01 12:41:38 -0300

    fix extensions

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import { BrowserRouter as Router } from 'react-router-dom';
-
 import Routes from './routes';
-
 import GlobalStyle from './styles/global';
 
-const App = () => (
-  <>
-    <GlobalStyle />
-    <Router>
-      <Routes />
-    </Router>
-  </>
-);
-
-export default App;
+export function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Router>
+        <Routes />
+      </Router>
+    </>
+  )
+}
