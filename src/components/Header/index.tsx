9cc2396e6bc0ba4f7c0f4dc5ea5b53af786a8d53
Patrick Morais <ppternunes@gmail.com>
2021-10-01 12:55:11 -0300

    refactored Header Component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,13 @@
-import { Component } from 'react';
 import { FiPlusSquare } from 'react-icons/fi';
 
 import { Container } from './styles';
 import Logo from '../../assets/logo.svg';
 
-class Header extends Component {
-  render() {
-    const { openModal } = this.props;
+interface HeaderProps {
+  openModal: () => void;
+}
+function Header(props:HeaderProps) {
+    const { openModal } = props;
 
     return (
       <Container>
@@ -28,7 +29,6 @@ class Header extends Component {
         </header>
       </Container>
     )
-  }
 };
 
 export default Header;
