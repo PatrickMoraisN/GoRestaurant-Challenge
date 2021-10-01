9fc9a8c730dbc5d85bbc41fbf0d0859b23f9c8a2
Patrick Morais <ppternunes@gmail.com>
2021-10-01 16:27:48 -0300

    refactored modalEditFood component

--- src/components/ModalEditFood/index.tsx
+++ src/components/ModalEditFood/index.tsx
@@ -4,27 +4,31 @@ import { FiCheckSquare } from 'react-icons/fi';
 import { Form } from './styles';
 import Modal from '../Modal';
 import Input from '../Input';
+import { FormHandles } from '@unform/core';
 
-class ModalEditFood extends Component {
-  constructor(props) {
-    super(props);
+interface ModalEditFoodProps {
+  isOpen: boolean;
+  setIsOpen: () => void;
+  editingFood: () => void;
+  handleUpdateFood: () => void;
+}
 
-    this.formRef = createRef()
-  }
+function ModalEditFood(props: ModalEditFoodProps) {
+    const formRef = createRef<FormHandles>()
 
-  handleSubmit = async (data) => {
-    const { setIsOpen, handleUpdateFood } = this.props;
+
+  const handleSubmit = async (data) => {
+    const { setIsOpen, handleUpdateFood } = props;
 
     handleUpdateFood(data);
     setIsOpen();
   };
 
-  render() {
-    const { isOpen, setIsOpen, editingFood } = this.props;
+    const { isOpen, setIsOpen, editingFood } = props;
 
     return (
       <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-        <Form ref={this.formRef} onSubmit={this.handleSubmit} initialData={editingFood}>
+        <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
           <h1>Editar Prato</h1>
           <Input name="image" placeholder="Cole o link aqui" />
 
@@ -42,7 +46,6 @@ class ModalEditFood extends Component {
         </Form>
       </Modal>
     );
-  }
 };
 
 export default ModalEditFood;
